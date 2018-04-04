88ad58b7fe28797e95a2fce1868c99c8537530ab
Joy <Joy@Joys-MacBook-Pro-2.local>
2018-04-03 23:29:22 -0400

    added images to explore, unfinished changes to PhotoDisplay.css

--- src/containers/Explore.js
+++ src/containers/Explore.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import PhotoDisplay from '../components/PhotoDisplay';
 import Navbar from './Navbar';
 import '../css/Explore.css';
-import clock from '../img/clock.png';
-import grid from '../img/grid.png';
+import annenberg from '../img/annenberg.jpg';
+import gate from '../img/gate.jpg';
+import yard from '../img/yard.jpg';
+import matthews from '../img/matthews.jpeg';
 
 // temp photo arrays
-let photos = [{img: clock, text: 'clock'}, {img: grid, text: 'grid'}];
+let photos = [{img: annenberg, text: 'annenberg'}, {img: gate, text: 'gate'}, {img: yard, text: 'yard'}, 
+	{img: matthews, text: 'matthews'}];
 
 class Profile extends Component {
     render() {
