6234914d5b48807c70fccaae39a935b20e85a7b4
Phillip Huang <philliphuang@college.harvard.edu>
2018-04-04 20:54:09 -0400

    full width in image viewer

--- src/components/ImageView.js
+++ src/components/ImageView.js
@@ -5,6 +5,7 @@ class ImageView extends Component {
     render() {
         return (
             <img 
+            	className='image'
             	src={this.props.image[0]} 
             	alt={this.props.image[1]} 
             	onClick={this.props.onClick}
