5c51772f0cb95e5c9e6cc9263b62660b62e80943
Joy <joyli@college.harvard.edu>
2018-04-11 16:23:57 -0400

    finished uploading images to tutorial and emoji history

--- src/components/EmojiHistory.js
+++ src/components/EmojiHistory.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import '../css/index.css';
 import '../css/EmojiHistory.css';
+import visualization from "../img/visualization.jpg"
 
 class EmojiHistory extends Component {
     render() {
         return (
 			<div className="visual">
-				<img src="../img/visualization.jpg"/>
+				<img src= {visualization} />
 			</div>
         );
     }
--- src/containers/Tutorial.js
+++ src/containers/Tutorial.js
@@ -8,7 +8,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { changeScreen } from '../actions'
 import '../css/Tutorial.css';
-import emojiscreen from "../img/emojiscreen.png"
+import emojiscreen from "../img/emojiscreen.png";
+import emojidisplay from "../img/emojidisplay.png";
+import quotescreen from "../img/quotescreen.png";
+import timescreen from "../img/timescreen.png";
+import profilescreen from "../img/profilescreen.png";
+import explorescreen from "../img/explorescreen.png";
+import camerascreen from "../img/camerascreen.png";
 
 class Tutorial extends Component {
     constructor(props) {
@@ -28,19 +34,19 @@ class Tutorial extends Component {
 				<div classNam="tutorial-steps">
 					<ol>
 						<li>Choose how you’re feeling!</li>
-							<img src={emojiscreen} />
-						<li>This is how you said you were feeling today!</li>
-							<img src="../img/emojidisplay.png"/>
+							<img className="tutorial-image" src={emojiscreen} alt="Screen with list of emojis: happy, sad, angry, disgusted, pizza."/>
+						<li>This is how you said you were feeling today! </li>
+							<img className="tutorial-image" src={emojidisplay} alt="Emoji is displayed on top right home screen corner." />
 						<li>This is your quote prompt!</li>
-							<img src="../img/quotescreen.png"/>
+							<img className="tutorial-image" src={quotescreen} alt="Quote text is in center of home screen." />
 						<li>This is how much time you have left to post.</li>
-							<img src="../img/timescreen.png"/>
-						<li>See your photos, wallpapers, and past emoji history here.</li>
-							<img src="../img/profilescreen.png"/>
+							<img className="tutorial-image" src={timescreen} alt="Clock with time left (e.g. X hours left!) is displayed underneath quote text." />
+						<li>See and download your photos, wallpapers, and past emoji history here.</li>
+							<img className="tutorial-image" src={profilescreen} alt="Icon with two photos in the bottom left corner of home screen."/>
 						<li>This is what other people have been posting!</li>
-							<img src="../img/explorescreen.png"/>
+							<img className="tutorial-image" src={explorescreen} alt="Icon with a grid of nine squares in the bottom right corner of home screen." />
 						<li>Swipe up to take photos and get started!</li>
-							<img src="../img/camerascreen.png"/>
+							<img className="tutorial-image" src={camerascreen} alt="Swipe up to access camera; icon located underneath time left in home screen." />
 
 					</ol>
 				</div>
