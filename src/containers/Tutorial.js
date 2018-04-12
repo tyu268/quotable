5bc8c88899ae37444c937f76192cb64be411726d
Phillip Huang <philliphuang@college.harvard.edu>
2018-04-11 21:41:40 -0400

    tutorial touchup

--- src/containers/Tutorial.js
+++ src/containers/Tutorial.js
@@ -15,6 +15,7 @@ import timescreen from "../img/timescreen.png";
 import profilescreen from "../img/profilescreen.png";
 import explorescreen from "../img/explorescreen.png";
 import camerascreen from "../img/camerascreen.png";
+import logo from "../img/logo.png";
 
 class Tutorial extends Component {
     constructor(props) {
@@ -30,29 +31,31 @@ class Tutorial extends Component {
 	render() {
 		return (
 			<div className="tutorial-screen">
-			<h1> Let's start with a quick overview... </h1>
-				<div className="tutorial-steps">
-					<ol>
-						<li>Choose how you’re feeling today!</li>
-							<img className="tutorial-image" src={emojiscreen} alt="Screen with list of emojis: happy, sad, angry, disgusted, pizza."/>
-						<li>This is how you said you were feeling today! Tap on it to change. </li>
-							<img className="tutorial-image" src={emojidisplay} alt="Emoji is displayed on top right home screen corner." />
-						<li>Take a photo inspired by the quote! We'll combine everyone's photos to make a cool wallpaper at the end of the day!</li>
-							<img className="tutorial-image" src={quotescreen} alt="Quote text is in center of home screen." />
-						<li>This is how much time you have left to post.</li>
-							<img className="tutorial-image" src={timescreen} alt="Clock with time left (e.g. X hours left!) is displayed underneath quote text." />
-						<li>See your photos, wallpapers, and past mood history here. Click on a photo to view and download!</li>
-							<img className="tutorial-image" src={profilescreen} alt="Icon with two photos in the bottom left corner of home screen."/>
-						<li>See what other people have been posting!</li>
-							<img className="tutorial-image" src={explorescreen} alt="Icon with a grid of nine squares in the bottom right corner of home screen." />
-						<li>Swipe up to launch the camera and get started!</li>
-							<img className="tutorial-image" src={camerascreen} alt="Swipe up to access camera; icon located underneath time left in home screen." />
+				<img className="logo" src={logo} alt="Quotable Logo"/>
+				<h1> Welcome to Quotable! </h1>
+				<h2> Let's start with a quick overview... </h2>
+					<div className="tutorial-steps">
+						<ol>
+							<li>Choose how you’re feeling today!</li>
+								<img className="tutorial-image" src={emojiscreen} alt="Screen with list of emojis: happy, sad, angry, disgusted, pizza."/>
+							<li>This is how you said you were feeling today! Tap on it to change. </li>
+								<img className="tutorial-image" src={emojidisplay} alt="Emoji is displayed on top right home screen corner." />
+							<li>Take a photo inspired by the quote! We'll combine everyone's photos to make a cool wallpaper at the end of the day!</li>
+								<img className="tutorial-image" src={quotescreen} alt="Quote text is in center of home screen." />
+							<li>This is how much time you have left to post.</li>
+								<img className="tutorial-image" src={timescreen} alt="Clock with time left (e.g. X hours left!) is displayed underneath quote text." />
+							<li>See your photos, wallpapers, and past mood history here. Click on a photo to view and download!</li>
+								<img className="tutorial-image" src={profilescreen} alt="Icon with two photos in the bottom left corner of home screen."/>
+							<li>See what other people have been posting!</li>
+								<img className="tutorial-image" src={explorescreen} alt="Icon with a grid of nine squares in the bottom right corner of home screen." />
+							<li>Swipe up to launch the camera and get started!</li>
+								<img className="tutorial-image" src={camerascreen} alt="Swipe up to access camera; icon located underneath time left in home screen." />
 
-					</ol>
-				</div>
-			<button className="bigbutton end-tutorial" data-name='home' onClick={this.handleClick}>
-                    Start Using Quotable!
-            </button>
+						</ol>
+					</div>
+				<button className="bigbutton end-tutorial" data-name='home' onClick={this.handleClick}>
+	                    Start Using Quotable!
+	            </button>
             </div>
 		);
 	}
